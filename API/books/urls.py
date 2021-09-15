ed30a2469f6ebd933ffbade4ab44c14f18635015
Garwend <kasprzak000@gmail.com>
2021-09-15 12:26:30 +0200

    books class view

--- API/books/urls.py
+++ API/books/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/', views.books_list, name='books-list'),
+    path('list/', views.BooksList.as_view(), name='books-list'),
     path('detail/<int:id>/', views.books_detail, name='book-detail'),
 ]
--- API/books/views.py
+++ API/books/views.py
@@ -1,22 +1,30 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .serializer import BookSerializer
 from.models import Book
 # Create your views here.
 
 
-@api_view(['GET'])
-def books_list(request):
-    if request.method == 'GET':
+class BooksList(APIView):
+    def get(self, request):
         books = Book.objects.all()
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def books_detail(request, id):
     if request.method == 'GET':
-        books = Book.objects.get(id=id)
+        books = get_object_or_404(Book, id=id)
         serializer = BookSerializer(books, many=False)
         return Response(serializer.data)
