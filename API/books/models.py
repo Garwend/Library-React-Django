c7e0032ee0f625a095ed38885499154221eb5459
Garwend <kasprzak000@gmail.com>
2021-09-21 14:21:18 +0200

    books model fix

--- API/books/models.py
+++ API/books/models.py
@@ -7,4 +7,3 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     author = models.CharField(max_length=255)
-    is_available = models.BooleanField(default=True)
